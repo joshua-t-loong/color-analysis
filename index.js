216de6080e6eb8d03b3ad768b61fbf6312a4f716
Joshua Loong <joshua.t.loong@gmail.com>
2017-06-17 12:10:55 -0400

    Playing around with the asynchronous vibrant function

--- index.js
+++ index.js
@@ -7,15 +7,21 @@ var options = {
   delimiter : ',' // optional
 };
 var dataset = csvjson.toObject(data, options);
+var colors = {};
 
 for (var i = 0; i < dataset.length; i++){
-  console.log(dataset[i].file);
   var v = new Vibrant('img/' + dataset[i].file)
   v.getPalette(function(e, s) {
   if (e) {
     console.log(e)
   } else {
-    console.log(JSON.stringify(s, null, 2))
+
+    colors = JSON.stringify(s, null, 2);
+    for (var i = 0; i < dataset.length; i++){
+      console.log(dataset[i].file);
+      console.log(colors[i] +"\n");
+
+    }
   }
 })
 }
