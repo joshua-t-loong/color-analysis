92cd0eac11a098326b3bd7ea8dbbfdcff2cc187a
Joshua Loong <joshua.t.loong@gmail.com>
2017-06-18 17:18:35 -0400

    Fixed reading colors to JSON file

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 var fs = require('fs');
 var Vibrant = require('node-vibrant');
+var color   = require('dominant-color');
 var csvjson = require('csvjson');
 var path    = require('path');
 var data = fs.readFileSync(path.join(__dirname, 'info.csv'), { encoding : 'utf8'});
@@ -7,21 +8,30 @@ var options = {
   delimiter : ',' // optional
 };
 var dataset = csvjson.toObject(data, options);
-var colors = {};
+var colors, colorsArray = [], count = 0;
 
 for (var i = 0; i < dataset.length; i++){
-  var v = new Vibrant('img/' + dataset[i].file)
+  var v = new Vibrant('img/' + dataset[i].file);
   v.getPalette(function(e, s) {
-  if (e) {
-    console.log(e)
-  } else {
-
-    colors = JSON.stringify(s, null, 2);
-    for (var i = 0; i < dataset.length; i++){
-      console.log(dataset[i].file);
-      console.log(colors[i] +"\n");
-
+    if (e) {
+      console.log(e);
     }
-  }
-})
+    else {
+     colors = JSON.stringify(s,null,2);
+     colorsArray.push(colors);
+    }
+  });
+}
+
+setTimeout(loadData, 10000, colorsArray);
+
+
+function loadData (colors)
+{
+  fs.writeFile("./colors.json", colors, function(err) {
+    if(err) {
+     return console.log(err);
+     }
+   console.log("The file was saved!");
+  });
 }
